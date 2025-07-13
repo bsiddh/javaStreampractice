9daa944786862e81ff6d8c5522bb83980b32ede2
bsiddh <bsiddh667@gmail.com>
2025-07-13 16:06:32 +0530

    new features added

--- src/com/streams/java/duplicateValues.java
+++ src/com/streams/java/duplicateValues.java
@@ -1,17 +1,28 @@
 package com.streams.java;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class duplicateValues {
 
     public boolean reOccuredValues(int[] num){
-        Set<Integer> arrayValues = new HashSet<>();
-return Arrays.stream(num)
-        .anyMatch(n->!arrayValues.add(n));
+        List<Integer> valueOfList = Arrays.stream(num)
+                .boxed()
+                .collect(Collectors.toList());
+
+        Set<Integer> arrayValues = new HashSet<>(valueOfList);
+        if(arrayValues.size()==valueOfList.size()){
+            return  false;
+        }
+        return  true;
+//return Arrays.stream(num)
+//        .anyMatch(n->!arrayValues.add(n));
+
     }
 
+
     public static void main(String[] args) {
-        int[] arrElement = {10,15,8,49,25,98,98,32,15};
+        int[] arrElement = {10,15,8,49,25,98,32,15};
         duplicateValues values = new duplicateValues();
         System.out.println(values.reOccuredValues(arrElement));
 
