1c19f06216a2008b7dbc59cd02208cb763bed186
bsiddh <bsiddh667@gmail.com>
2025-07-25 19:40:48 +0530

    all the examples

--- src/com/lambdaexpression/java/ExampleOne.java
+++ src/com/lambdaexpression/java/ExampleOne.java
@@ -11,8 +11,17 @@ interface Greeting
 public class ExampleOne {
 
     public static void main(String[] args) {
-        Greeting gone = ()->{
+        Greeting gone = ()->
             System.out.println("lambda expression first example");
+            gone.greet();
+
+        Greeting gones = () -> {
+            System.out.println("Values added");
+            System.out.println("values not added");
+
         };
+        gones.greet();
+
+
     }
 }
