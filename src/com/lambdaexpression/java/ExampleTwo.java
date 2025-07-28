5a4fd8e2527b8a97d38cdb00645248588ae080cf
bsiddh <bsiddh667@gmail.com>
2025-07-28 16:44:52 +0530

    changes have been made

--- src/com/lambdaexpression/java/ExampleTwo.java
+++ src/com/lambdaexpression/java/ExampleTwo.java
@@ -11,7 +11,17 @@ public class ExampleTwo {
         Operations o1 = (int valOne,int valTwo) ->System.out.println("Additon of two values :"+(valOne+valTwo));
         o1.add(10,10);
 
+
+
+
         Operations o2 = (valOne, valTwo) -> System.out.println(valOne+valTwo);
         o2.add(88,92);
     }
 }
+
+
+
+
+
+
+
